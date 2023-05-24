1105ea5f829855a31a7e326aaf4307b1de743df0
shouviksarkar02 <113041705+shouviksarkar02@users.noreply.github.com>
2023-05-25 00:04:40 +0530

    Update taskControllers.js

--- controllers/taskControllers.js
+++ controllers/taskControllers.js
@@ -1,109 +1,99 @@
-const Users   = require("../models/user.js");
+const Users = require("../models/user.js");
 const jwt = require("jsonwebtoken");
-const Tasks   = require("../models/task.js");
-const bcrypt  = require('bcrypt');
+const Tasks = require("../models/task.js");
+const bcrypt = require("bcrypt");
 const { valid } = require("joi");
 const JWT_SECRET = "newtonSchool";
 
+const createTask = async (req, res) => {
+  //creator_id is user id who have created this task.
 
-const createTask =async (req, res) => {
+  const { heading, description, token } = req.body;
+  let decodedToken;
+  try {
+    decodedToken = jwt.verify(token, JWT_SECRET);
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: "Invalid token",
+    });
+  }
+  const creator_id = decodedToken.userId;
 
-    //creator_id is user id who have created this task.
+  const newtask = {
+    heading,
+    description,
+    creator_id,
+  };
 
-    const { heading, description, token  } = req.body;
-    let decodedToken;
-    try{
-        decodedToken = jwt.verify(token, JWT_SECRET);
-    }catch(err){
-        res.status(404).json({
-            status: 'fail',
-            message: 'Invalid token'
-        });
-    }
-    const creator_id = decodedToken.userId;
-
-    const newtask = {
-        heading,
-        description,
-        creator_id
-    };
-
-    try{
-        const task = await Tasks.create(newtask);
-        res.status(200).json({
-            message: 'Task added successfully',
-            task_id: task._id,
-            status: 'success'
-        });
-    }catch(error){
-        res.status(404).json({
-            status: 'fail',
-            message: error.message
-        });
-    };
-
-}
-
+  try {
+    const task = await Tasks.create(newtask);
+    res.status(200).json({
+      message: "Task added successfully",
+      task_id: task._id,
+      status: "success",
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: "fail",
+      message: error.message,
+    });
+  }
+};
 
 const getdetailTask = async (req, res) => {
+  const task_id = req.body.task_id;
 
-    const task_id = req.body.task_id;
-
-    try{
-        const task = await Tasks.findById(task_id);
-        res.status(200).json({
-            status: 'success',
-            data: task
-        })
-    }catch(err){
-        res.status(404).json({
-            status: 'fail',
-            message: err.message
-        })
-    }
-}
+  try {
+    const task = await Tasks.findById(task_id);
+    res.status(200).json({
+      status: "success",
+      data: task,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err.message,
+    });
+  }
+};
 
 const updateTask = async (req, res) => {
-    
-    const task_id = req.body.task_id;
-    try{
-        const task = await Tasks.findByIdAndUpdate(
-            task_id,
-            { $set:  req.body },
-            { new: true }
-        );
-        res.status(200).json({
-            status:'success',
-            data: task
-        });
-    }catch(err){
-        res.status(404).json({
-            status:'fail',
-            data: err.message
-        });
-    }
-
-}
-
+  const task_id = req.body.task_id;
+  try {
+    const task = await Tasks.findByIdAndUpdate(
+      task_id,
+      { $set: req.body },
+      { new: true }
+    );
+    res.status(200).json({
+      status: "success",
+      data: task,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      data: err.message,
+    });
+  }
+};
 
 const deleteTask = async (req, res) => {
+  const { task_id, token } = req.body;
 
-    const {task_id, token} = req.body;
-
-    try{
-        await Tasks.findByIdAndDelete(task_id);
-        res.status(200).json({
-            status: 'success',
-            message: 'Task deleted successfully'
-        });
-    }catch(err){
-        res.status(404).json({
-            status: 'fail',
-            message: err.message
-        })
-    };
-
-}
+  try {
+    await Tasks.findByIdAndDelete(task_id);
+    res.status(200).json({
+      status: "success",
+      message: "Task deleted successfully",
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err.message,
+    });
+  }
+};
 
 /*
 
@@ -135,34 +125,49 @@ the latest data will be at the top.
 */
 
 const getallTask = async (req, res) => {
+  const { token } = req.body;
+  //Write your code here.
 
-    //Write your code here.
-    
-    const { token  } = req.body;
-    let decodedToken;
-    try{
-        decodedToken = jwt.verify(token, JWT_SECRET);
-    }catch(err){
-        res.status(404).json({
-            status: 'fail',
-            message: 'Invalid token'
-        });
+  try {
+    const decodedToken = jwt.verify(token, JWT_SECRET);
+    const user = await Users.findById(decodedToken.userId);
+    // if (!user) {
+    //   return res.status(404).json({
+    //     status: "error",
+    //     message: "User not found",
+    //   });
+    // }
+
+    let tasks = [];
+
+    if (user.role === "admin") {
+      tasks = await Tasks.find().sort({ createdAt: -1 });
+    } else {
+      tasks = await Tasks.find({ creator_id: user._id }).sort({
+        createdAt: -1,
+      });
     }
-    const creator_id = decodedToken.userId;
-    
-    const user= await Users.findById({creator_id})
-    
-    if(user.role==='admin'){
-        const alltask= await Tasks.find({}).sort({timestamps: -1})
-        
-        res.status(200).json({status:'success', data: [alltask]})
-    }else{
-        const task= await Tasks.find({creator_id:creator_id}).sort({timestamps: -1})
-        res.status(200).json({status:'success', data: [task]})
+
+    const { status } = req.query;
+    if (status) {
+      tasks = tasks.filter((task) => task.status === status);
     }
-    
-    
-}
+    res.status(200).json({
+      status: "success",
+      data: tasks,
+    });
+  } catch (error) {
+    return res.status(404).json({
+      status: "error",
+      message: "User not found",
+    });
+  }
+};
 
-
-module.exports = { createTask, getdetailTask, updateTask, deleteTask, getallTask };
+module.exports = {
+  createTask,
+  getdetailTask,
+  updateTask,
+  deleteTask,
+  getallTask,
+};
